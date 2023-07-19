e3609b1a06b738a34954cd7a51325657ef9ddcd0
Julien Berthomier <berthomier.julien@gmail.com>
2023-07-19 18:16:46 +0100

    Dashboard app

--- my_app/dashboard.py
+++ my_app/dashboard.py
@@ -9,7 +9,7 @@ import google.auth
 import google.auth.transport.requests
 import google.oauth2.service_account
 
-key_path = "covid-dashboard-378011-d39bea98e1ae.json"
+key_path = "my_app/covid-dashboard-378011-d39bea98e1ae.json"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
 
 client = bigquery.Client(location="US", project="covid-dashboard-378011")
