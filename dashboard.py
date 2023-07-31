889394e7da1b7de25cbd150bdb57469dbfab3023
Julien Berthomier <berthomier.julien@gmail.com>
2023-07-31 18:18:01 +0100

    Deploy render

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,10 @@ import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
+import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
 from google.cloud import bigquery
 import os
 import json
@@ -35,7 +38,99 @@ df_2 = client.query(query).to_dataframe()
 
 
 # Build the layout for the Dash app
+# Create some random data
 
+# Create some random data
+np.random.seed(0)
+df = pd.DataFrame({
+    'Category': ['A', 'B', 'C', 'D', 'E'] * 200,
+    'Value': np.random.rand(1000),
+    'X': np.random.rand(1000),
+    'Y': np.random.rand(1000),
+})
+
+# Define the layout
+app = dash.Dash(__name__)
+app.layout = html.Div(children=[
+    dcc.Graph(
+        id='line-chart',
+        figure={
+            'data': [
+                go.Scatter(x=df['X'], y=df['Y'], mode='lines', name='Line Chart')
+            ]
+        }
+    ),
+    dcc.Graph(
+        id='bar-chart',
+        figure={
+            'data': [
+                go.Bar(x=df['Category'], y=df['Value'], name='Bar Chart')
+            ]
+        }
+    ),
+    dcc.Graph(
+        id='scatter-plot',
+        figure={
+            'data': [
+                go.Scatter(x=df['X'], y=df['Y'], mode='markers', name='Scatter Plot')
+            ]
+        }
+    ),
+    dcc.Graph(
+        id='histogram',
+        figure={
+            'data': [
+                go.Histogram(x=df['Value'], name='Histogram')
+            ]
+        }
+    ),
+    dcc.Graph(
+        id='pie-chart',
+        figure={
+            'data': [
+                go.Pie(labels=df['Category'], values=df['Value'], name='Pie Chart')
+            ]
+        }
+    ),
+    dcc.Graph(
+        id='box-plot',
+        figure={
+            'data': [
+                go.Box(x=df['Category'], y=df['Value'], name='Box Plot')
+            ]
+        }
+    ),
+    dcc.Graph(
+        id='heatmap',
+        figure={
+            'data': [
+                go.Heatmap(z=df.corr(), x=df.columns, y=df.columns, name='Heatmap')
+            ]
+        }
+    ),
+    dcc.Graph(
+        id='violin-plot',
+        figure={
+            'data': [
+                go.Violin(y=df['Value'], x=df['Category'], name='Violin Plot')
+            ]
+        }
+    ),
+    dcc.Graph(
+        id='area-chart',
+        figure={
+            'data': [
+                go.Scatter(x=df['X'], y=df['Y'], mode='lines', fill='tozeroy', name='Area Chart')
+            ]
+        }
+    ),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+'''
 app = dash.Dash(__name__)
 server = app.server
 
@@ -66,3 +161,5 @@ app.layout = html.Div(children=[
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+'''
